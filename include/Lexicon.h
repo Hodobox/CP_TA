1d3edeec62d7b6fdd5465915ef63e6f85f5232dd
Hodobox <samkogursky@gmail.com>
2020-05-02 16:56:01 +0100

    add project base

---
+++ include/Lexicon.h
@@ -0,0 +1,16 @@
+#ifndef LEXICON_H
+#define LEXICON_H
+
+
+class Lexicon
+{
+    public:
+        Lexicon();
+        virtual ~Lexicon();
+
+    protected:
+
+    private:
+};
+
+#endif // LEXICON_H
---
+++ src/Lexicon.cpp
@@ -0,0 +1,11 @@
+#include "Lexicon.h"
+
+Lexicon::Lexicon()
+{
+    //ctor
+}
+
+Lexicon::~Lexicon()
+{
+    //dtor
+}
