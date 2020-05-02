f1050564910a60c1aeb06d1095a7ab5905ef78f0
Hodobox <samkogursky@gmail.com>
2020-05-02 23:33:32 +0100

    add FinishRoom POC

--- include/Room.h
+++ include/Room.h
@@ -20,7 +20,7 @@ class Room
         Level* level;
 
         string enter_desc;
-        bool enter();
+        virtual bool enter();
         void list_neighbors();
 
     protected:
@@ -32,5 +32,12 @@ enum directions { north, east, south, west };
 const string dir_names[4] = { "North", "East", "South", "West" };
 
 bool make_neighbors(Room* first, Room* second, int direction);
+
+class FinishRoom : public Room
+{
+    public:
+        FinishRoom(string name) : Room(name) {};
+        bool enter() override;
+};
 
 #endif // ROOM_H
